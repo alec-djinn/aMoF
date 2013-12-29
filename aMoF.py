f5c6ae42dd83b394bd568ced66b0187cde6f6c78
alec-djinn <alec_djinn@yahoo.com>
2013-12-29 20:08:48 +0100

    Update aMoF.py

--- aMoF.py
+++ aMoF.py
@@ -1,22 +1,4 @@
-#############################***********alec's Motif Finder***********############################
-#                                                                                                #
-# The aim of this software is to provide a full featured 'motif finder' focused on the analysis  #
-# of typical APTAMER selection results. Aptamers are short sequence of nucleotides or aminoacids #
-# (DNA, RNA, peptides, circular peptides and small proteins). In molecular biology, there are    #
-# two main methods to select aptamers: SELEX and Phage Display. Both methods rely on the         #
-# analysis of sequences to discriminate specifically enriched aptamers from background noise.    #
-# The input of this software is then a list of short sequences of DNA, RNA or peptides.          #
-# The lenght of such sequences is tipically in a range of 5 and 50 units. The sequences are      #
-# composed exclusively by strings of alphabet characters.                                        #
-#                                                                                                #
-##################################################################################################
- 
- 
- 
- 
- 
- 
- 
+
 def merge_sequences(in_file, out_file):
 	'''(input file name, output file name) -> dict of {'id':'sequence'}, ['merged_sequences'], output_file
 	Read a fasta format and store its data in a dictionary with >name as key and sequence as value.

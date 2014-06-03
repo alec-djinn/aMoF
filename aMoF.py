46ce810ac19e48a01f21d63e7f5a438802887c72
alec-djinn <alec_djinn@yahoo.com>
2014-06-03 16:58:56 +0200

    Some minor improvements
    Solved a issue occurring when sequencing files of length 0bp are
    present.

--- aMoF.py
+++ aMoF.py
@@ -1,18 +1,17 @@
-#functions
+# functions
 def checkPython():
 	'''(none) -> str
 	It checks Python version (only the major int) and prints what has been found. 
 	'''
 	version_check = int((sys.version)[0]) # checks Python version
 	if version_check == 2:
-		#execute code for Python 2
+		# execute code for Python 2
 		print('Detected Python 2')
 	elif version_check == 3:
-		#execute code for Python 2
+		# execute code for Python 2
 		print('Detected Python 3')
 	else:
 		sys.exit('Python version error!!\nThis program is compatible not compatible with your Python version.\nPlease install Python 2.7.x or Python 3.x')		
-
 
 
 def formatSequences(infile):
@@ -28,25 +27,23 @@ def formatSequences(infile):
 	with open(infile, 'r') as infile:
 		line = infile.readline()
 		while line != '':
-			if line != '\n' and '>>>' not in line: #skip initial blank lines and '>>>' tagged lines
+			if line != '\n' and '>>>' not in line: # skip initial blank lines and '>>>' tagged lines
 				id = line[0:].strip().upper()
-				#count += 1
 				line = infile.readline()
-				while line == '\n' or '>>>' in line: #skip blank and '>>>' tagged lines between id and sequence
+				while line == '\n' or '>>>' in line: # skip blank and '>>>' tagged lines between id and sequence
 					line = infile.readline()
 				sequence = line[0:].strip().upper()
 				if sequence != 'WILD TYPE' and sequence != 'CORRUPTED':
 					print(id + '\n' + sequence + '\n')
 					count += 1
 				line = infile.readline()
-				if id not in sequence_dict: #check if the id are unique
+				if id not in sequence_dict: # check if the id are unique
 					sequence_dict[id] = sequence
 				else:
 					sys.exit(str('File format error: ' + id + ' is not an unique sequence id.' + '\nPlease check the sequence file and try again.'))
 			else:
 				line = infile.readline()
 	print('>>>total sequences found ' + str(count) + '\n')			
-
 
 
 def findMotifs(infile, motif_len, repetition):
@@ -58,36 +55,30 @@ def findMotifs(infile, motif_len, repetition):
 	Sorts the dictionary and prints a report.
 	Precondition: each sequence in the file is above its id. 
 	'''
-	#sequence_dict = {}
 	motif_dict = {}
 	with open(infile, 'r') as infile:
 		line = infile.readline()
 		while line != '':
-			if line != '\n' and '>>>' not in line: #skip initial blank lines and '>>>' tagged lines
-				id = line[0:].strip().lower() #set id in lowercase
+			if line != '\n' and '>>>' not in line: # skip initial blank lines and '>>>' tagged lines
+				id = line[0:].strip().lower() # set id in lowercase
 				line = infile.readline()
-				while line == '\n' or '>>>' in line: #skip blank and '>>>' tagged lines between id and sequence
+				while line == '\n' or '>>>' in line: # skip blank and '>>>' tagged lines between id and sequence
 					line = infile.readline()	
-				sequence = line[0:].strip().upper()	#set sequence in uppercase
-				#print(id + '\n' + sequence + '\n')
-				for i in range(len(sequence) - motif_len +1): #generate a dictionary of motifs (motif_dict)
+				sequence = line[0:].strip().upper()	# set sequence in uppercase
+				for i in range(len(sequence) - motif_len +1): # generate a dictionary of motifs (motif_dict)
 					motif = sequence[i:i+motif_len]
 					if motif not in motif_dict:
 						motif_dict[motif] = 1
 					else:
 						motif_dict[motif] += 1
 				line = infile.readline()
-				#if id not in sequence_dict:
-					#sequence_dict[id] = sequence
-				#else:
-					#sys.exit(str('File format error: ' + id + ' is not an unique sequence id.' + '\nPlease check the sequence file and try again.'))
 			else:
 				line = infile.readline()
-	#remove from motif_dict all the motif repeated less than 'repetition' times
+	# remove from motif_dict all the motif repeated less than 'repetition' times
 	keys_to_remove = [key for key, value in motif_dict.items() if value < repetition]
 	for key in keys_to_remove:
 		del motif_dict[key]
-	#sort the dictionary
+	# sort the dictionary
 	global sorted_list #set sorted_list as global variable
 	sorted_list = sorted(motif_dict.items(), key = operator.itemgetter(1), reverse = True)
 	print('>>>detected motifs with lenght = ' + str(motif_len) + ' and repeated at least ' + str(repetition) + ' times')
@@ -95,22 +86,21 @@ def findMotifs(infile, motif_len, repetition):
 	print('\n')			
 
 
-
-#imports	
+# imports	
 import operator
 import sys
 import time
 
-#variables
+# variables
 infile = 'sequence_file.txt'
 logfile = 'results.txt'
-motif_len = 5
-repetition = 4
+motif_len = 3
+repetition = 0
 write_to_log = 'yes' #if yes, it prints the output in logfile instead printing on screen
 logfile = 'results.txt'
 
 
-#write output - part 1
+# write output - part 1
 if write_to_log == 'yes':
 	old_stdout = sys.stdout
 	logfile = open(logfile,'w')
@@ -118,7 +108,7 @@ if write_to_log == 'yes':
 else:
 	pass
 
-#body
+# body
 start_time = time.time()
 checkPython()
 formatSequences(infile)
@@ -129,14 +119,14 @@ while sorted_list != []:
 
 print('>>>execution time :' + str(time.time() - start_time) + ' seconds\n')
 
-#write output - part 2
+# write output - part 2
 if write_to_log == 'yes':
-	sys.stdout = old_stdout #log END
-	logfile.close() #close log_file
-	#exit
+	sys.stdout = old_stdout # log END
+	logfile.close() # close log_file
+	# exit
 	sys.exit('>>>program ran succesfully and its output has been written in log.txt')
 else:
 	pass
 	
-#exit
+# exit
 sys.exit('>>>program ran succesfully')
